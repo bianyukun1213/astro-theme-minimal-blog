bea6bda6de38466314938a8cdb0cdced27fab8f3
Hollis <bianyukun1213@outlook.com>
2026-05-16 18:54:53 +0800

    add @astrojs/ cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -23,102 +23,112 @@ import { DEFAULT_LOCALE, LOCALES, SITE } from './src/constants'
 import { remarkAsides } from './src/remark'
 import { pagefindIntegration } from './src/serverUtils'
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
-	fonts: [
-		{
-			name: 'Noto Serif',
-			cssVariable: '--font-noto-serif',
-			provider: fontProviders.google(),
-			weights: [400, 500, 600],
-			styles: ['normal', 'italic'],
-			fallbacks: [],
-			optimizedFallbacks: false,
-		},
-		{
-			name: 'Noto Serif SC',
-			cssVariable: '--font-noto-serif-sc',
-			provider: fontProviders.google(),
-			weights: [400, 500, 600],
-			styles: ['normal', 'italic'],
-			fallbacks: [],
-			optimizedFallbacks: false,
-		},
-		{
-			name: 'Noto Serif Hebrew',
-			cssVariable: '--font-noto-serif-hebrew',
-			provider: fontProviders.google(),
-			weights: [400, 500, 600],
-			styles: ['normal', 'italic'],
-			fallbacks: [],
-			optimizedFallbacks: false,
-		},
+  fonts: [
+      {
+          name: 'Noto Serif',
+          cssVariable: '--font-noto-serif',
+          provider: fontProviders.google(),
+          weights: [400, 500, 600],
+          styles: ['normal', 'italic'],
+          fallbacks: [],
+          optimizedFallbacks: false,
+      },
+      {
+          name: 'Noto Serif SC',
+          cssVariable: '--font-noto-serif-sc',
+          provider: fontProviders.google(),
+          weights: [400, 500, 600],
+          styles: ['normal', 'italic'],
+          fallbacks: [],
+          optimizedFallbacks: false,
+      },
+      {
+          name: 'Noto Serif Hebrew',
+          cssVariable: '--font-noto-serif-hebrew',
+          provider: fontProviders.google(),
+          weights: [400, 500, 600],
+          styles: ['normal', 'italic'],
+          fallbacks: [],
+          optimizedFallbacks: false,
+      },
 	],
-	output: 'static',
-	trailingSlash: 'always',
-	site: SITE.url,
-	base: SITE.base,
-	integrations: [sitemap(), alpinejs({ entrypoint: '/src/entrypoint' }), react(), pagefindIntegration(), expressiveCode(), mdx()],
-	vite: {
-		plugins: [
-			tailwindcss(),
-			tailwindResolver({
-				input: './src/styles/global.css', // Your Tailwind CSS file
-			}),
-			paraglideVitePlugin({
-				project: './project.inlang',
-				outdir: './src/paraglide',
-				emitTsDeclarations: true,
-			}),
-		],
+
+  output: 'static',
+  trailingSlash: 'always',
+  site: SITE.url,
+  base: SITE.base,
+  integrations: [sitemap(), alpinejs({ entrypoint: '/src/entrypoint' }), react(), pagefindIntegration(), expressiveCode(), mdx()],
+
+  vite: {
+      plugins: [
+          tailwindcss(),
+          tailwindResolver({
+              input: './src/styles/global.css', // Your Tailwind CSS file
+          }),
+          paraglideVitePlugin({
+              project: './project.inlang',
+              outdir: './src/paraglide',
+              emitTsDeclarations: true,
+          }),
+      ],
 	},
-	image: {
-		responsiveStyles: true,
-		layout: 'constrained',
+
+  image: {
+      responsiveStyles: true,
+      layout: 'constrained',
 	},
-	i18n: {
-		locales: LOCALES as unknown as string[],
-		defaultLocale: DEFAULT_LOCALE,
-		routing: {
-			prefixDefaultLocale: true,
-			redirectToDefaultLocale: true,
-		},
+
+  i18n: {
+      locales: LOCALES as unknown as string[],
+      defaultLocale: DEFAULT_LOCALE,
+      routing: {
+          prefixDefaultLocale: true,
+          redirectToDefaultLocale: true,
+      },
 	},
-	devToolbar: {
-		enabled: false,
+
+  devToolbar: {
+      enabled: false,
 	},
-	markdown: {
-		// @ts-expect-error: Astro types don't match remark plugin
-		remarkPlugins: [[remarkSmartypants, { backticks: false }], remarkDirective, remarkAsides, [remarkToc, { heading: SITE.tocHeading }], [remarkCollapse, { test: SITE.tocHeading, summary: 'btn_expand_toc_title' }], [remarkSandpack, { componentName: ['Playground'] }]],
-		rehypePlugins: [
-			rehypeSlug,
-			[
-				rehypeExternalLinks,
-                {
+
+  markdown: {
+      // @ts-expect-error: Astro types don't match remark plugin
+      remarkPlugins: [[remarkSmartypants, { backticks: false }], remarkDirective, remarkAsides, [remarkToc, { heading: SITE.tocHeading }], [remarkCollapse, { test: SITE.tocHeading, summary: 'btn_expand_toc_title' }], [remarkSandpack, { componentName: ['Playground'] }]],
+      rehypePlugins: [
+          rehypeSlug,
+          [
+              rehypeExternalLinks,
+              {
                 	target: '_blank',
                 	rel: ['nofollow'],
                 	properties: { className: ['external_link'] },
-                } satisfies ExternalLinkOptions,
-			],
-			[
-				rehypeAutolinkHeadings,
-                {
+              } satisfies ExternalLinkOptions,
+          ],
+          [
+              rehypeAutolinkHeadings,
+              {
                 	behavior: 'after',
                 	group() {
-                		return h('.markdown-heading')
+                      return h('.markdown-heading')
                 	},
                 	headingProperties() {
-                		return { tabIndex: -1 }
+                      return { tabIndex: -1 }
                 	},
                 	properties(node: any) {
-                		return { ariaLabel: toString(node), className: 'anchor' }
+                      return { ariaLabel: toString(node), className: 'anchor' }
                 	},
                 	content() {
-                		return h('svg', { className: 'anchor-icon', viewBox: '0 0 16 16', ariaHidden: true }, [
-                			s('path', { d: 'm7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z' }),
-                		])
+                      return h('svg', { className: 'anchor-icon', viewBox: '0 0 16 16', ariaHidden: true }, [
+                          s('path', { d: 'm7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z' }),
+                      ])
                 	},
-                } satisfies AutolinkHeadingsOptions,
-			],
-		],
+              } satisfies AutolinkHeadingsOptions,
+          ],
+      ],
 	},
+
+  adapter: cloudflare(),
 })
