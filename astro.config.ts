67a1e85905afe625447900e016f86e38c40374b8
Hollis <bianyukun1213@outlook.com>
2026-05-21 22:48:42 +0800

    optimize styles

--- astro.config.ts
+++ astro.config.ts
@@ -17,6 +17,7 @@ import rehypeExternalLinks from 'rehype-external-links'
 import rehypeSlug from 'rehype-slug'
 import remarkCollapse from 'remark-collapse'
 import remarkDirective from 'remark-directive'
+import { remarkRehypeWrap } from 'remark-rehype-wrap'
 import remarkSmartypants from 'remark-smartypants'
 import remarkToc from 'remark-toc'
 import { tailwindResolver } from 'tailwind-resolver/vite'
@@ -100,31 +101,38 @@ export default defineConfig({
 			rehypeSlug,
 			[
 				rehypeExternalLinks,
-              {
-              	target: '_blank',
-              	rel: ['nofollow'],
-              	properties: { className: ['external_link'] },
-              } satisfies ExternalLinkOptions,
+				{
+					target: '_blank',
+					rel: ['nofollow'],
+					properties: { className: ['external_link'] },
+				} satisfies ExternalLinkOptions,
 			],
 			[
 				rehypeAutolinkHeadings,
-              {
-              	behavior: 'after',
-              	group() {
-              		return h('.markdown-heading')
-              	},
-              	headingProperties() {
-              		return { tabIndex: -1 }
-              	},
-              	properties(node: any) {
-              		return { ariaLabel: toString(node), className: 'anchor' }
-              	},
-              	content() {
-              		return h('svg', { className: 'anchor-icon', viewBox: '0 0 16 16', ariaHidden: true }, [
-              			s('path', { d: 'm7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z' }),
-              		])
-              	},
-              } satisfies AutolinkHeadingsOptions,
+				{
+					behavior: 'after',
+					group() {
+						return h('.markdown-heading')
+					},
+					headingProperties() {
+						return { tabIndex: -1 }
+					},
+					properties(node: any) {
+						return { ariaLabel: toString(node), className: 'anchor' }
+					},
+					content() {
+						return h('svg', { className: 'anchor-icon', viewBox: '0 0 16 16', ariaHidden: true }, [
+							s('path', { d: 'm7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z' }),
+						])
+					},
+				} satisfies AutolinkHeadingsOptions,
+			],
+			[
+				remarkRehypeWrap,
+				{
+					node: { type: 'element', tagName: 'div', properties: { className: 'tide-table-wrapper' } },
+					start: 'element[tagName=table]',
+				},
 			],
 		],
 	},
